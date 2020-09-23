9861420d5134cec3c83d7f4a6fca4ac68be985b7
Vasee <babu.vasee@accenture.com>
2020-09-23 17:14:34 -0400

    first version

---
+++ A.java
@@ -0,0 +1,13 @@
+class A{  
+static public void main(String... args){  
+System.out.println("hello world"  ); 
+ 
+try{
+System.out.println( 1/0 ); 
+}catch (Exception e){
+System.out.println( e ); 
+
+}
+
+}  
+}
